cbe086446c404887f6b82d7bec4f711e6034aa4a
Hongs1024 <67249792+Hongs1024@users.noreply.github.com>
2021-03-04 10:54:08 +0900

    HPlugin

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 어셈퍼리에 관한 일반 정보는 아래의 속성 세트를 통해 제어됩니다.
+// 어셈블리에 관련된 정보를 변경하려면,
+// 이러한 속성값을 변경해주세요.
+[assembly: AssemblyTitle("SanyaPlugin")]
+[assembly: AssemblyDescription("SCP:SL Smod Plugin")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("sanyae2439")]
+[assembly: AssemblyProduct("SanyaPlugin")]
+[assembly: AssemblyCopyright("Copyright ©  2019 sanyae2439")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible을 false로 설정하면 그 형태는 어셈블리 내에서 COM 컴포넌트에서
+// 참조불가능하게됩니다.COM에서 이 어셈블리 내의 틀에 액세스하는 경우에는
+// 그 형태의 ComVisible 속성을 true로 설정해 주세요.
+[assembly: ComVisible(false)]
+
+// 이 프로젝트가 COM에 공개될 경우, 다음의 GUID가 typelib 의 ID가 됩니다.
+[assembly: Guid("e59cf3a0-1741-469a-b1f3-7063135d0652")]
+
+// 어셈블리의 버전 정보는 다음 4가지 값으로 구성되어 있습니다:
+//
+//      메이저 버전
+//      마이너 버전
+//      빌드 번호
+//      수정사항
+//
+// 모든 값을 지정하거나 아래와 같이 '*' 를 사용하여 빌드 및 리비전 번호를
+// 기본값으로 할 수 있습니다.
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("13.6.6.4")]
+[assembly: AssemblyFileVersion("13.6.6.4")]
+[assembly: NeutralResourcesLanguage("ja-JP")]
